ac806b1b503b951e35707dcf81ddd22b82c89c4e
NikPrime <nikprime1@gmail.com>
2019-11-16 16:19:10 +0300

    Fix warnings and checkstyle

--- code/distance-between-vectors/src/main/java/ru/unn/agile/distanceBetweenVectors/model/VectorMetric.java
+++ code/distance-between-vectors/src/main/java/ru/unn/agile/distanceBetweenVectors/model/VectorMetric.java
@@ -6,27 +6,29 @@ import java.util.Vector;
 public class VectorMetric {
     private float sumOfModules = 0f;
 
-    public float Calculate_L1(final Vector x) {
+    public float calculateL1(final Vector<Float> x) {
         x.forEach((elem) -> sumOfModules += Math.abs((float) elem));
         return sumOfModules;
     }
 
-    public float Calculate_L2(final Vector x) {
-        x.forEach((elem) -> sumOfModules += (float) elem*(float) elem);
-        return (float)Math.sqrt(sumOfModules);
+    public float calculateL2(final Vector<Float> x) {
+        x.forEach((elem) -> sumOfModules += Math.pow((float) elem, 2.0));
+        return (float) Math.sqrt(sumOfModules);
     }
 
-    public float Calculate_L3(final Vector x) {
-        x.forEach((elem) -> sumOfModules += Math.abs(Math.pow((float)elem, 3.0)));
-        return (float)Math.pow((float)sumOfModules, 1.0/3.0);
+    public float calculateL3(final Vector<Float> x) {
+        final float pow = 3.0f;
+        x.forEach((elem) -> sumOfModules += Math.abs(Math.pow((float) elem, pow)));
+        return (float) Math.pow((float) sumOfModules, 1.0 / pow);
     }
 
-    public float Calculate_L4(final Vector x) {
-        x.forEach((elem) -> sumOfModules += Math.pow((float)elem, 4.0));
-        return (float)Math.pow((float)sumOfModules, 1.0/4.0);
+    public float calculateL4(final Vector<Float> x) {
+        final float pow = 4.0f;
+        x.forEach((elem) -> sumOfModules += Math.pow((float) elem, pow));
+        return (float) Math.pow((float) sumOfModules, 1.0 / pow);
     }
 
-    public float Calculate_Linf(final Vector x) {
+    public float calculateLinf(final Vector<Float> x) {
         return (float) Collections.max(x);
     }
 }
--- code/distance-between-vectors/src/test/java/ru/unn/agile/distanceBetweenVectors/model/TestMetrics.java
+++ code/distance-between-vectors/src/test/java/ru/unn/agile/distanceBetweenVectors/model/TestMetrics.java
@@ -8,137 +8,137 @@ import static org.junit.Assert.*;
 
 public class TestMetrics {
     @Test
-    public void canCalculateMetricOneDimensionalVector_L1() {
+    public void canCalculateMetricOneDimensionalVectorL1() {
         Vector<Float> v = new Vector<Float>();
         v.add(-2.2f);
-        float metricL1 = new VectorMetric().Calculate_L1(v);
+        float metricL1 = new VectorMetric().calculateL1(v);
         assertEquals(2.2f, metricL1, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricTwoDimensionalVector_L1() {
+    public void canCalculateMetricTwoDimensionalVectorL1() {
         Vector<Float> v = new Vector<Float>();
         v.add(3.3f);
         v.add(-6.7f);
-        float metricL1 = new VectorMetric().Calculate_L1(v);
+        float metricL1 = new VectorMetric().calculateL1(v);
         assertEquals(10f, metricL1, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricThreeDimensionalVector_L1() {
+    public void canCalculateMetricThreeDimensionalVectorL1() {
         Vector<Float> v = new Vector<Float>();
         v.add(1.0f);
         v.add(5.8f);
         v.add(-0.3f);
-        float metricL1 = new VectorMetric().Calculate_L1(v);
+        float metricL1 = new VectorMetric().calculateL1(v);
         assertEquals(7.1f, metricL1, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricOneDimensionalVector_L2() {
+    public void canCalculateMetricOneDimensionalVectorL2() {
         Vector<Float> v = new Vector<Float>();
         v.add(-2.5f);
-        float metricL2 = new VectorMetric().Calculate_L2(v);
+        float metricL2 = new VectorMetric().calculateL2(v);
         assertEquals(2.5f, metricL2, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricTwoDimensionalVector_L2() {
+    public void canCalculateMetricTwoDimensionalVectorL2() {
         Vector<Float> v = new Vector<Float>();
         v.add(1.0f);
         v.add(-2.0f);
-        float metricL2 = new VectorMetric().Calculate_L2(v);
+        float metricL2 = new VectorMetric().calculateL2(v);
         assertEquals(2.23606797749979f, metricL2, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricThreeDimensionalVector_L2() {
+    public void canCalculateMetricThreeDimensionalVectorL2() {
         Vector<Float> v = new Vector<Float>();
         v.add(-3.0f);
         v.add(7.1f);
         v.add(0.5f);
-        float metricL2 = new VectorMetric().Calculate_L2(v);
+        float metricL2 = new VectorMetric().calculateL2(v);
         assertEquals(7.723988606931007f, metricL2, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricOneDimensionalVector_L3() {
+    public void canCalculateMetricOneDimensionalVectorL3() {
         Vector<Float> v = new Vector<Float>();
         v.add(-2.0f);
-        float metricL3 = new VectorMetric().Calculate_L3(v);
+        float metricL3 = new VectorMetric().calculateL3(v);
         assertEquals(2.0f, metricL3, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricTwoDimensionalVector_L3() {
+    public void canCalculateMetricTwoDimensionalVectorL3() {
         Vector<Float> v = new Vector<Float>();
         v.add(2.0f);
         v.add(-3.0f);
-        float metricL3 = new VectorMetric().Calculate_L3(v);
+        float metricL3 = new VectorMetric().calculateL3(v);
         assertEquals(3.2710663101886f, metricL3, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricThreeDimensionalVector_L3() {
+    public void canCalculateMetricThreeDimensionalVectorL3() {
         Vector<Float> v = new Vector<Float>();
         v.add(2.0f);
         v.add(2.5f);
         v.add(-4.0f);
-        float metricL3 = new VectorMetric().Calculate_L3(v);
+        float metricL3 = new VectorMetric().calculateL3(v);
         assertEquals(4.4416330599643f, metricL3, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricOneDimensionalVector_L4() {
+    public void canCalculateMetricOneDimensionalVectorL4() {
         Vector<Float> v = new Vector<Float>();
         v.add(-2.0f);
-        float metricL4 = new VectorMetric().Calculate_L4(v);
+        float metricL4 = new VectorMetric().calculateL4(v);
         assertEquals(2.0f, metricL4, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricTwoDimensionalVector_L4() {
+    public void canCalculateMetricTwoDimensionalVectorL4() {
         Vector<Float> v = new Vector<Float>();
         v.add(-2.0f);
         v.add(1.0f);
-        float metricL4 = new VectorMetric().Calculate_L4(v);
+        float metricL4 = new VectorMetric().calculateL4(v);
         assertEquals(2.0305431848689f, metricL4, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricThreeDimensionalVector_L4() {
+    public void canCalculateMetricThreeDimensionalVectorL4() {
         Vector<Float> v = new Vector<Float>();
         v.add(-2.0f);
         v.add(-2.0f);
         v.add(-2.1f);
-        float metricL4 = new VectorMetric().Calculate_L4(v);
+        float metricL4 = new VectorMetric().calculateL4(v);
         assertEquals(2.678195865012f, metricL4, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricOneDimensionalVector_Linf() {
+    public void canCalculateMetricOneDimensionalVectorLinf() {
         Vector<Float> v = new Vector<Float>();
         v.add(-2.0f);
-        float metricLinf = new VectorMetric().Calculate_Linf(v);
+        float metricLinf = new VectorMetric().calculateLinf(v);
         assertEquals(-2.0f, metricLinf, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricTwoDimensionalVector_Linf() {
+    public void canCalculateMetricTwoDimensionalVectorLinf() {
         Vector<Float> v = new Vector<Float>();
         v.add(4.0f);
         v.add(-100.0f);
-        float metricLinf = new VectorMetric().Calculate_Linf(v);
+        float metricLinf = new VectorMetric().calculateLinf(v);
         assertEquals(4.0f, metricLinf, 0.0001);
     }
 
     @Test
-    public void canCalculateMetricThreeDimensionalVector_Linf() {
+    public void canCalculateMetricThreeDimensionalVectorLinf() {
         Vector<Float> v = new Vector<Float>();
         v.add(-55.0f);
         v.add(-2.0f);
         v.add(150.1f);
-        float metricLinf = new VectorMetric().Calculate_Linf(v);
+        float metricLinf = new VectorMetric().calculateLinf(v);
         assertEquals(150.1f, metricLinf, 0.0001);
     }
 }
