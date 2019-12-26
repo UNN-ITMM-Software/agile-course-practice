a1392f00a4a00ceefd24f29e565532025c2a950f
sakhmedov <s.akhmedov@mfisoft.ru>
2019-12-26 16:12:48 +0300

    Fixed variable names

--- code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/AkhmedovSarkarTest.java
+++ code/korniakov-kirill-lab1/src/test/java/ru/unn/agile/ComplexNumber/Model/AkhmedovSarkarTest.java
@@ -9,25 +9,25 @@ public class AkhmedovSarkarTest {
 
     @Test
     public void canSumNumbers() {
-        ComplexNumber с1 = new ComplexNumber(1.3, 0.45);
-        ComplexNumber с2 = new ComplexNumber(10, 5.5);
-        ComplexNumber sum = с1.add(с2);
+        ComplexNumber cn1 = new ComplexNumber(1.3, 0.45);
+        ComplexNumber cn2 = new ComplexNumber(10, 5.5);
+        ComplexNumber sum = cn1.add(cn2);
         assertEquals(sum, new ComplexNumber(11.3, 5.95));
     }
 
     @Test
     public void canSumNumbersWithNegatuivePart() {
-        ComplexNumber с1 = new ComplexNumber(1.3, -0.45);
-        ComplexNumber с2 = new ComplexNumber(-10, 5.5);
-        ComplexNumber sum = с1.add(с2);
+        ComplexNumber cn1 = new ComplexNumber(1.3, -0.45);
+        ComplexNumber cn2 = new ComplexNumber(-10, 5.5);
+        ComplexNumber sum = cn1.add(cn2);
         assertEquals(sum, new ComplexNumber(-8.7, 5.05));
     }
 
     @Test
     public void cantSumNumbersWithDiffInitialization() {
-        ComplexNumber с1 = new ComplexNumber("1", "1");
-        ComplexNumber с2 = new ComplexNumber(2, 5);
-        ComplexNumber sum = с1.add(с2);
+        ComplexNumber cn1 = new ComplexNumber("1", "1");
+        ComplexNumber cn2 = new ComplexNumber(2, 5);
+        ComplexNumber sum = cn1.add(cn2);
         assertEquals(sum, new ComplexNumber(3, 6));
     }
 }
