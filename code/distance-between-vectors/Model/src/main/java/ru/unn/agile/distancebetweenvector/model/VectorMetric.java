1938bc3b99b4926a71e6f9c2ed0f355fafbd3d62
NikPrime <nikprime1@gmail.com>
2020-01-26 17:43:20 +0300

    Change structure of Model

---
+++ code/distance-between-vectors/Model/src/main/java/ru/unn/agile/distancebetweenvector/model/VectorMetric.java
@@ -0,0 +1,43 @@
+package ru.unn.agile.distancebetweenvector.model;
+
+import java.util.Collections;
+import java.util.Vector;
+
+public class VectorMetric {
+    private float sumOfModulesL1 = 0f;
+    private float sumOfModulesL2 = 0f;
+    private float sumOfModulesL3 = 0f;
+    private float sumOfModulesL4 = 0f;
+    public float calculateL1(final Vector<Float> x) {
+        x.forEach((elem) -> sumOfModulesL1 += Math.abs((float) elem));
+        return sumOfModulesL1;
+    }
+
+    public float calculateL2(final Vector<Float> x) {
+        x.forEach((elem) -> sumOfModulesL2 += Math.pow((float) elem, 2.0));
+        return (float) Math.sqrt(sumOfModulesL2);
+    }
+
+    public float calculateL3(final Vector<Float> x) {
+        final float pow = 3.0f;
+        x.forEach((elem) -> sumOfModulesL3 += Math.abs(Math.pow((float) elem, pow)));
+        return (float) Math.pow((float) sumOfModulesL3, 1.0 / pow);
+    }
+
+    public float calculateL4(final Vector<Float> x) {
+        final float pow = 4.0f;
+        x.forEach((elem) -> sumOfModulesL4 += Math.pow((float) elem, pow));
+        return (float) Math.pow((float) sumOfModulesL4, 1.0 / pow);
+    }
+
+    public float calculateLinf(final Vector<Float> x) {
+        return (float) Collections.max(x);
+    }
+}
+
+
+
+
+
+
+
---
+++ code/distance-between-vectors/Model/src/test/java/ru/unn/agile/distancebetweenvector/model/TestMetrics.java
@@ -0,0 +1,194 @@
+package ru.unn.agile.distancebetweenvector.model;
+
+import org.junit.Test;
+import java.util.Vector;
+
+import static org.junit.Assert.*;
+
+public class TestMetrics {
+    private final double comparisonDELTA = 0.0001;
+    @Test
+    public void canCalculateMetricOneDimensionalVectorL1() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = -2.2f;
+        final float expectedL1 = 2.2f;
+        v.add(coordX);
+        float metricL1 = new VectorMetric().calculateL1(v);
+        assertEquals(expectedL1, metricL1, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricTwoDimensionalVectorL1() {
+        final float coordX = 3.3f;
+        final float coordY = -6.7f;
+        final float expectedL1 = 10f;
+        Vector<Float> v = new Vector<Float>();
+        v.add(coordX);
+        v.add(coordY);
+        float metricL1 = new VectorMetric().calculateL1(v);
+        assertEquals(expectedL1, metricL1, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricThreeDimensionalVectorL1() {
+        final float coordX = 1.0f;
+        final float coordY = 5.8f;
+        final float coordZ = -0.3f;
+        final float expectedL1 = 7.1f;
+        Vector<Float> v = new Vector<Float>();
+        v.add(coordX);
+        v.add(coordY);
+        v.add(coordZ);
+        float metricL1 = new VectorMetric().calculateL1(v);
+        assertEquals(expectedL1, metricL1, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricOneDimensionalVectorL2() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = -2.5f;
+        final float expectedL2 = 2.5f;
+        v.add(coordX);
+        float metricL2 = new VectorMetric().calculateL2(v);
+        assertEquals(expectedL2, metricL2, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricTwoDimensionalVectorL2() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = 1.0f;
+        final float coordY = -2.0f;
+        final float expectedL2 = 2.23606797749979f;
+        v.add(coordX);
+        v.add(coordY);
+        float metricL2 = new VectorMetric().calculateL2(v);
+        assertEquals(expectedL2, metricL2, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricThreeDimensionalVectorL2() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = -3.0f;
+        final float coordY = 7.1f;
+        final float coordZ = 0.5f;
+        final float expectedL2 = 7.723988606931007f;
+        v.add(coordX);
+        v.add(coordY);
+        v.add(coordZ);
+        float metricL2 = new VectorMetric().calculateL2(v);
+        assertEquals(expectedL2, metricL2, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricOneDimensionalVectorL3() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = -2.0f;
+        final float expectedL3 = 2.0f;
+        v.add(coordX);
+        float metricL3 = new VectorMetric().calculateL3(v);
+        assertEquals(expectedL3, metricL3, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricTwoDimensionalVectorL3() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = 2.0f;
+        final float coordY = -3.0f;
+        final float expectedL3 = 3.2710663101886f;
+        v.add(coordX);
+        v.add(coordY);
+        float metricL3 = new VectorMetric().calculateL3(v);
+        assertEquals(expectedL3, metricL3, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricThreeDimensionalVectorL3() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = 2.0f;
+        final float coordY = 2.5f;
+        final float coordZ = -4.0f;
+        final float expectedL3 = 4.4416330599643f;
+        v.add(coordX);
+        v.add(coordY);
+        v.add(coordZ);
+        float metricL3 = new VectorMetric().calculateL3(v);
+        assertEquals(expectedL3, metricL3, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricOneDimensionalVectorL4() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = -2.0f;
+        final float expectedL4 = 2.0f;
+        v.add(coordX);
+        float metricL4 = new VectorMetric().calculateL4(v);
+        assertEquals(expectedL4, metricL4, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricTwoDimensionalVectorL4() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = -2.0f;
+        final float coordY = 1.0f;
+        final float expectedL4 = 2.0305431848689f;
+        v.add(coordX);
+        v.add(coordY);
+        float metricL4 = new VectorMetric().calculateL4(v);
+        assertEquals(expectedL4, metricL4, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricThreeDimensionalVectorL4() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = -2.0f;
+        final float coordY = -2.0f;
+        final float coordZ = -2.1f;
+        final float expectedL4 = 2.678195865012f;
+        v.add(coordX);
+        v.add(coordY);
+        v.add(coordZ);
+        float metricL4 = new VectorMetric().calculateL4(v);
+        assertEquals(expectedL4, metricL4, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricOneDimensionalVectorLinf() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = -2.0f;
+        final float expectedLinf = -2.0f;
+        v.add(coordX);
+        float metricLinf = new VectorMetric().calculateLinf(v);
+        assertEquals(expectedLinf, metricLinf, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricTwoDimensionalVectorLinf() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = 4.0f;
+        final float coordY = -100.0f;
+        final float expectedLinf = 4.0f;
+        v.add(coordX);
+        v.add(coordY);
+        float metricLinf = new VectorMetric().calculateLinf(v);
+        assertEquals(expectedLinf, metricLinf, comparisonDELTA);
+    }
+
+    @Test
+    public void canCalculateMetricThreeDimensionalVectorLinf() {
+        Vector<Float> v = new Vector<Float>();
+        final float coordX = -55.0f;
+        final float coordY = -2.0f;
+        final float coordZ = 150.1f;
+        final float expectedLinf = 150.1f;
+        v.add(coordX);
+        v.add(coordY);
+        v.add(coordZ);
+        float metricLinf = new VectorMetric().calculateLinf(v);
+        assertEquals(expectedLinf, metricLinf, comparisonDELTA);
+    }
+}
+
+
+
+
+
