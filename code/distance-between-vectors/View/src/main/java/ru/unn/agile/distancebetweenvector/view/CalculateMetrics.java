03bf9af17e46742709602a45d1824be761e1abbf
NikPrime <nikprime1@gmail.com>
2020-01-26 17:45:59 +0300

    Add View in MVVM

---
+++ code/distance-between-vectors/View/src/main/java/ru/unn/agile/distancebetweenvector/view/CalculateMetrics.java
@@ -0,0 +1,21 @@
+package ru.unn.agile.distancebetweenvector.view;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CalculateMetrics {
+    @FXML
+    private TextField coordX;
+    @FXML
+    private TextField coordY;
+    @FXML
+    private TextField coordZ;
+    @FXML
+    private Button btnCalc;
+    @FXML
+    void initialize() {
+    }
+}
